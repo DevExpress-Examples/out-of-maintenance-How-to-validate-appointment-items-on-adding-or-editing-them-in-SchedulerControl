ebc814c1eb1f63e8874293943caacbcc112ba20f
DevExpressExampleBot <fakeEmail>
2020-01-28 16:13:50 +0300

    Source auto update [skip ci]

---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Namespace EntityFrameworkCodeFirstBindingExample
+	Partial Public Class MainWindow
+		Inherits System.Windows.Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/ValidationClasses/WarningUserControl.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Controls
+
+Namespace EntityFrameworkCodeFirstBindingExample
+	Partial Public Class WarningUserControl
+		Inherits UserControl
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
