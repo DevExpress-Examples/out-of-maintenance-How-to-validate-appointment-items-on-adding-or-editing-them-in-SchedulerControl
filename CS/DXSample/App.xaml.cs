1f3ab3e07afa1b23c7ae6bc9774519023b151a34
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-02-14 15:34:27 +0300

    Add CS content

---
+++ CS/DXSample/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Windows;
+using DevExpress.Internal;
+using DXSample.Data;
+
+namespace DXSample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            DbEngineDetector.PatchConnectionStringsAndConfigureEntityFrameworkDefaultConnectionFactory();
+            Database.SetInitializer<SchedulingContext>(new SchedulingContextInitializer());
+            base.OnStartup(e);
+        }
+    }
+}
