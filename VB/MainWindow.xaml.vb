e1aed22ac9e434cadda1bf3c903a5f75f6502028
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-02-14 15:33:53 +0300

    Delete previous implementation

--- VB/MainWindow.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Namespace EntityFrameworkCodeFirstBindingExample
-	Partial Public Class MainWindow
-		Inherits System.Windows.Window
-
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
-End Namespace
